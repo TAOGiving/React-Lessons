a774debed5cc6c1f0ce76d67985b4c21f82e362f
Alex Williams <axelwill@live.com>
2025-04-27 22:37:49 +0100

    React Props

--- 01-starting-project/01-starting-project/src/App.jsx
+++ 01-starting-project/01-starting-project/src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from "./assets/react-core-concepts.png";
+import ComponentsImg from "./assets/components.png";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -20,12 +21,36 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
-      <Header></Header>
+      <Header />
+
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The code UI building block"
+              image={ComponentsImg}
+            />
+
+            <CoreConcept />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
       </main>
     </div>
   );
