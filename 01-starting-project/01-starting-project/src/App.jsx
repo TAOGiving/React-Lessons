6ad564ebddd9b7fd0e5ece67017e2686574f3dd9
Alex Williams <axelwill@live.com>
2025-04-27 22:53:56 +0100

    Destructuring props

--- 01-starting-project/01-starting-project/src/App.jsx
+++ 01-starting-project/01-starting-project/src/App.jsx
@@ -1,5 +1,6 @@
 import reactImg from "./assets/react-core-concepts.png";
-import ComponentsImg from "./assets/components.png";
+// import ComponentsImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data.js";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -21,12 +22,12 @@ function Header() {
   );
 }
 
-function CoreConcept(props) {
+function CoreConcept({ title, description, image }) {
   return (
     <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   );
 }
@@ -41,14 +42,14 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             <CoreConcept
-              title="Components"
-              description="The code UI building block"
-              image={ComponentsImg}
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
             />
-
-            <CoreConcept />
-            <CoreConcept />
-            <CoreConcept />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            {/* Same as above but shorter */}
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
       </main>
